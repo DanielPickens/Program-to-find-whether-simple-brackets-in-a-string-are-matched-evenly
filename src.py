b4cdd4982a2899f7f97169a46ad3b86921a01044
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-11 09:09:52 -0700

    commit: 2
    kjzkj

--- src.py
+++ src.py
@@ -14,3 +14,26 @@ def balance(myStr):
     if len(stack) == 0:
         return "Balanced"
 print balance("{[()](){}}")
+
+
+
+
+####second solution counts and matches parenthesis from string: 
+
+def do_parentheses_match(input_string):
+    s = []
+    balanced = True
+    index = 0
+    while index < len(input_string) and balanced:
+        token = input_string[index]
+        if token == "(":
+            s.append(token)
+        elif token == ")":
+            if len(s) == 0:
+                balanced = False
+            else:
+                s.pop()
+
+        index += 1
+
+    return balanced and len(s) == 0
